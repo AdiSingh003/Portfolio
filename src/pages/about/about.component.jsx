15c3fc250554fed1a67f456d01afe80a320d51fc
AdiSingh003 <aprataps710@gmail.com>
2024-04-06 23:35:52 +0530

    portfolio changes commit

---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/Aditya_Pratap_Singh.jpg";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import Tilt from "react-tilt";
+
+const About = () => {
+  return (
+    <div>
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2">
+                <Tilt options={{ max: 5, scale: 1 }}>
+                  <Image
+                    className="profile justify-content-end"
+                    alt="profile"
+                    src={Profile}
+                    thumbnail
+                    fluid
+                  />
+                </Tilt>
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi there! I am <strong>&nbsp;Aditya Pratap Singh</strong>
+                <br />I am an IT engineer with experience in Software development
+                with the latest technologies. I adapt to working with colleagues and
+                customers of all backgrounds by being a Teambuilder and a clear
+                communicator with strong problem-solving and planning skills. Also
+                being a sports enthusiast, I manage a local club named Benchmark
+                Sports Academy and has been a part of the soccer team of CBI
+                and HDFC Bank.
+                <br /> <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://drive.google.com/file/d/153Mp_UPGYEe2SZHAckm0BZTYGLceaWUJ/view?usp=drive_link"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://github.com/AdiSingh003"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://www.linkedin.com/in/aditya-pratap-singh-a82a62279/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/skills/skills-data.js
@@ -0,0 +1,114 @@
+import L_HTML5 from "../../assets/img/skills/html-5.svg";
+import L_CSS3 from "../../assets/img/skills/css3.svg";
+import L_SASS from "../../assets/img/skills/sass-1.svg";
+import L_BOOTSTRAP from "../../assets/img/skills/bootstrap-4.svg";
+import L_REACT from "../../assets/img/skills/react.svg";
+import L_REDUX from "../../assets/img/skills/redux.svg";
+import L_REACT_ROUTER from "../../assets/img/skills/react-router.svg";
+import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
+import L_REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
+import L_STYLED_COMPONENTS from "../../assets/img/skills/styled-components.svg";
+import L_NODE_JS from "../../assets/img/skills/nodejs.svg";
+import L_EXPRESS from "../../assets/img/skills/express.svg";
+import L_MONGODB from "../../assets/img/skills/mongodb.svg";
+import L_POSTGRESQL from "../../assets/img/skills/postgresql.svg";
+import L_MSSQL from "../../assets/img/skills/mssql.svg";
+import L_GIT from "../../assets/img/skills/git-icon.svg";
+import L_HEROKU from "../../assets/img/skills/heroku.svg";
+import L_DIGITAL_OCEAN from "../../assets/img/skills/digital-ocean.svg";
+import L_GITHUB_PAGES from "../../assets/img/skills/github.svg";
+import L_JAVASCRIPT from "../../assets/img/skills/javascript.svg";
+import L_TYPESCRIPT from "../../assets/img/skills/typescript.svg";
+import L_MYSQL from "../../assets/img/skills/mysql-logo-svgrepo-com.svg";
+import L_PYTHON from "../../assets/img/skills/python.svg";
+import L_PHP from "../../assets/img/skills/php-svgrepo-com.svg";
+import L_VSCODE from "../../assets/img/skills/vscode-svgrepo-com.svg";
+import L_POSTMAN from "../../assets/img/skills/postman-icon-svgrepo-com.svg";
+import L_NESTJS from "../../assets/img/skills/nestjs.svg";
+import L_FIREBASE from "../../assets/img/skills/firebase.svg";
+import L_FLUTTER from "../../assets/img/skills/flutter.svg";
+import L_Dart from "../../assets/img/skills/dart.svg";
+
+export const skills = {
+  frontend: [
+    {
+      imgAltText: "HTML 5",
+      imgSrc: L_HTML5,
+      skillName: "HTML5",
+    },
+    {
+      imgAltText: "CSS 3",
+      imgSrc: L_CSS3,
+      skillName: "CSS3",
+    },
+    {
+      imgAltText: "JavaScript",
+      imgSrc: L_JAVASCRIPT,
+      skillName: "JavaScript",
+    },
+    {
+      imgAltText: "Bootstrap",
+      imgSrc: L_BOOTSTRAP,
+      skillName: "Bootstrap",
+    },
+    {
+      imgAltText: "React JS",
+      imgSrc: L_REACT,
+      skillName: "React JS",
+    },
+  ],
+  backend: [
+    {
+      imgAltText: "PHP",
+      imgSrc: L_PHP,
+      skillName: "PHP",
+    },
+    {
+      imgAltText: "Python",
+      imgSrc: L_PYTHON,
+      skillName: "Python",
+    },
+  ],
+  hostingPlatforms: [
+    {
+      imgAltText: "GitHub Pages",
+      imgSrc: L_GITHUB_PAGES,
+      skillName: "GitHub Pages",
+    },
+  ],
+  
+  databases: [
+    {
+      imgAltText: "MongoDB",
+      imgSrc: L_MONGODB,
+      skillName: "MongoDB",
+    },
+    {
+      imgAltText: "MySQL",
+      imgSrc: L_MYSQL,
+      skillName: "MySQL",
+    },
+  ],
+  Others: [
+    {
+      imgAltText: "GIT",
+      imgSrc: L_GIT,
+      skillName: "GIT",
+    },
+    {
+      imgAltText: "GITHUB",
+      imgSrc: L_GITHUB_PAGES,
+      skillName: "GITHUB",
+    },
+    {
+      imgAltText: "VS COde",
+      imgSrc: L_VSCODE,
+      skillName: "VS Code",
+    },
+    {
+      imgAltText: "Postman",
+      imgSrc: L_POSTMAN,
+      skillName: "Postman",
+    },
+  ],
+};
